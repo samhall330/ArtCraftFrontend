09c092f5b164874f854934db8479cd21934d2e37
Sam Hall <mann.samantha11@gmail.com>
2021-03-31 22:17:35 -0700

    ReadMe

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ function About(){
     return (
     <>
     <h5 className="display-4">ArtCraft is a virtual artist collective in which users can create projects and invite other users as collaborators.
-    <img className="about-img" src="redblue.gif"></img>
+    {/* <img className="about-img" src="redblue.gif"></img> */}
     </h5>
     </>
     )
