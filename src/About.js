b3c86f987d31f81937c29e7798920194cfb24c5b
Sam Hall <mann.samantha11@gmail.com>
2021-09-20 22:59:59 -0400

    fixed about page graphic

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ function About(){
     return (
     <>
     <h5 className="display-4">ArtCraft is a virtual artist collective. Create a new project and find collaborators who share your passion.</h5>
-    <img className="about-img" src="redblue.gif"></img>
+    <img className="about-img" src="redblue.gif" height="500px" width="1275px"></img>
     </>
     )
 
